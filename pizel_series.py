f8c53731f9991ede961f02c3b0d5b2211c05d2f6
Megan Mason <meganmason491@boisestate.edu>
2019-05-23 09:27:30 -0600

    more functions and main().

--- pizel_series.py
+++ pizel_series.py
@@ -7,52 +7,64 @@ from matplotlib import pyplot as plt
 # ~~~~~~~~~~~~~~~~~~~~   functions  ~~~~~~~~~~~~~~~~~~~~~~~~~
 #select pixel
 def get_closest(arr,pt):
+    """
+    This grabs the closest index to a point
+    """
     idx = np.argmin(np.abs(arr-pt)) #indexs location
     return idx
 
-# def get_depth()
 #get snow depth
+def get_depth(ds,ix_min,iy_min):
+    arr = ds['Band1'][:,:]
+    scaled_dep = arr[ix_min,iy_min] #gets recaled depth value
+    # dep.append(scaled_dep)
+    return scaled_dep
 #write to file (apend to list)
 
 
+def main():
+    #open and loop netCDF lidar files
+    path = ('/home/meganmason/Documents/projects/thesis/data/processing_lidar/'
+    'depths_3m/all_rescaled/nc_lidar/')
 
-#open and loop netCDF lidar files
-path = '/home/meganmason/Documents/projects/thesis/data/processing_lidar/\
-depths_3m/all_rescaled/nc_lidar/'
+    pt_x = 4190721.0 #lon
+    pt_y = 301751.9 #lat
 
-pt_x = 4190721.0 #lon
-pt_y = 301751.9 #lat
+    dep = []
+    # idx_x = []
+    # idx_y = []
+    for filepath in sorted(glob.glob(os.path.join(path, '*.nc'))):
+        print(os.path.basename(filepath))
+        # print(filepath.split("/")[-1])
+        """ here's the netCDF dataset """
+        ds = Dataset(filepath)
+        lat = ds.variables['x'][:] #lat values(degrees) to np arrays
+        lon = ds.variables['y'][:]
 
-dep = []
-# idx_x = []
-# idx_y = []
+        """ find pixel for x,y point"""
+        ix_min = get_closest(lon, pt_x)
+        iy_min = get_closest(lat, pt_y)
 
-for filepath in sorted(glob.glob(os.path.join(path, '*.nc'))):
-    ds = Dataset(filepath)
-    lat = ds.variables['x'][:] #lat values(degrees) to np arrays
-    lon = ds.variables['y'][:]
-
-    ix_min = get_closest(lon, pt_x)
-    iy_min = get_closest(lat, pt_y)
-
-    arr = ds['Band1'][:,:]
-    scaled_dep = arr[ix_min,iy_min] #gets recaled depth value
-
-    dep.append(scaled_dep)
-
+        """ what is the depth (or rescalled depth) @ pixel """
+        j=get_depth(ds,ix_min,iy_min)
+        ds.close()
+        dep.append(j)
 
 
+
+# fig, ax = plt.subplots()
+# ax.plot(dep)
 #
-fig, ax = plt.subplots()
-ax.plot(dep)
-
-ax.set(xlabel='recaled snow depth', ylabel='freqency',
-       title='pixel time series')
-ax.grid()
-
-# fig.savefig("test.png")
-plt.show()
+# ax.set(xlabel='recaled snow depth', ylabel='freqency',
+#        title='pixel time series')
+# ax.grid()
+#
+# # fig.savefig("test.png")
+# plt.show()
 
 
 # plt.plot(dep)
 # plt.show()
+
+if __name__ == '__main__':
+    main()
